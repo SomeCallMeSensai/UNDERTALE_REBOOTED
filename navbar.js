e5ce91e39c269eda5226a7fe9fb4af3ce6a90470
Sensational <109011523+SomeCallMeSensai@users.noreply.github.com>
2022-07-10 13:14:40 -0500

    UPDATE_2

--- navbar.js
+++
@@ -1,9 +0,0 @@
-document.write("    <nav class="navbar">");
-document.write("      <div>");
-document.write("        <a href="index.html">HOME</a>");
-document.write("        <a href="index.html">ABOUT</a>");
-document.write("        <a href="progress.html">GANEJOLT</a>");
-document.write("        <a href="soundtrack.html">SOUNDTRACK</a>");
-document.write("        <a href="credits.html">CREDITS</a>");
-document.write("      </div>");
-document.write("    </nav>");
